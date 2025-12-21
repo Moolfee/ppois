a80b4d9beb3e3bec3bc2c3f7bed52b640b97b9fe
Moolfee <romandenisovic@gmail.com>
2025-12-21 19:35:36 +0300

    updated additional 15 lab exaples

--- LAB_ADD/LAB_ADD_15/main.cpp
+++ LAB_ADD/LAB_ADD_15/main.cpp
@@ -15,22 +15,41 @@ void printGridWithPath(const AStar::Grid &grid, const AStar::Path &path) {
     std::cout << row << '\n';
   }
 }
+
+void runCase(const std::string &title, const AStar::Grid &grid,
+             AStar::Point start, AStar::Point goal) {
+  std::cout << title << "\n";
+  auto path = AStar::findPath(grid, start, goal);
+  if (path.empty()) {
+    std::cout << "Path not found\n\n";
+    return;
+  }
+  std::cout << "Path length: " << path.size() - 1 << "\n\n";
+  printGridWithPath(grid, path);
+  std::cout << "\n";
+}
 }
 
 int main() {
-  AStar::Grid grid = {"........", "..####..", "..#.....", "..#.###.",
-                      "..#...#.", "..###.#.", "......#.", "........"};
+  runCase("Case 1",
+          {"........", "..####..", "..#.....", "..#.###.", "..#...#.",
+           "..###.#.", "......#.", "........"},
+          {0, 0}, {7, 7});
 
-  AStar::Point start{0, 0};
-  AStar::Point goal{7, 7};
+  runCase("Case 2",
+          {"............", "..####..####", "..#....#....", "..#....#....",
+           "..#....#....", "..#....#....", "..#....#....", "..#....#....",
+           "..#....#....", ".####..####.", "#..........."},
+          {0, 0}, {10, 11});
 
-  auto path = AStar::findPath(grid, start, goal);
-  if (path.empty()) {
-    std::cout << "Path not found\n";
-    return 0;
-  }
-
-  std::cout << "Path length: " << path.size() - 1 << "\n\n";
-  printGridWithPath(grid, path);
+  runCase("Case 3",
+          {"...............#", ".#####.########..",
+           ".#...#.......#...", ".#.#.#####.#.#...",
+           ".#.#.....#.#...#", ".#.#.###.#.###...",
+           ".#...#.#.#...#...", ".###.#.#.###.#...",
+           ".#...#...#...#...", ".#.#.#####.#.###.",
+           ".#.#.....#.....#.", ".#####.#####.##..",
+           "#................"},
+          {0, 0}, {12, 16});
   return 0;
 }
